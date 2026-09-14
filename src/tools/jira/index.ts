f96893d6803e8cfc26c2091f6b791f8ddf42f52c
GryphLabs-Ziggy <ziggy@gryphlabs.com>
2026-09-14 16:51:26 -0500

    refactor: expose only approved Jira tools

--- src/tools/jira/index.ts
+++ src/tools/jira/index.ts
@@ -1,23 +1,15 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { IAtlassianClient } from "../../http-client.js";
-import { registerJiraSearch } from "./search.js";
-import { registerJiraGetIssue } from "./get-issue.js";
-import { registerJiraCreateIssue } from "./create-issue.js";
-import { registerJiraUpdateIssue } from "./update-issue.js";
-import { registerJiraTransitionIssue } from "./transition-issue.js";
-import { registerJiraAddComment } from "./add-comment.js";
-import { registerJiraListProjects } from "./list-projects.js";
 import { registerJiraGetConfluenceLinks } from "./get-confluence-links.js";
-import { registerJiraCreateIssueLink } from "./create-issue-link.js";
+import { registerJiraGetIssue } from "./get-issue.js";
+import { registerJiraListProjects } from "./list-projects.js";
+import { registerJiraMoveIssue } from "./move-issue.js";
+import { registerJiraSearch } from "./search.js";
 
 export function registerJiraTools(server: McpServer, client: IAtlassianClient) {
   registerJiraSearch(server, client);
   registerJiraGetIssue(server, client);
-  registerJiraCreateIssue(server, client);
-  registerJiraUpdateIssue(server, client);
-  registerJiraTransitionIssue(server, client);
-  registerJiraAddComment(server, client);
   registerJiraListProjects(server, client);
   registerJiraGetConfluenceLinks(server, client);
-  registerJiraCreateIssueLink(server, client);
+  registerJiraMoveIssue(server, client);
 }
