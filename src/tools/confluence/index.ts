8a1c25540d1cc3c4d51670e7cd2222fcb1faaf00
GryphLabs-Ziggy <ziggy@gryphlabs.com>
2026-09-14 16:51:31 -0500

    refactor: make Confluence tools read-only

--- src/tools/confluence/index.ts
+++ src/tools/confluence/index.ts
@@ -1,13 +1,9 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { IAtlassianClient } from "../../http-client.js";
 import { registerConfluenceGetPage } from "./get-page.js";
-import { registerConfluenceSearch } from "./search.js";
 import { registerConfluenceGetPageChildren } from "./get-page-children.js";
-import { registerConfluenceCreatePage } from "./create-page.js";
-import { registerConfluenceUpdatePage } from "./update-page.js";
-import { registerConfluenceUpdatePageDiff } from "./update-page-diff.js";
 import { registerConfluenceGetPageComments } from "./get-page-comments.js";
-import { registerConfluenceAddPageComment } from "./add-page-comment.js";
+import { registerConfluenceSearch } from "./search.js";
 
 export function registerConfluenceTools(
   server: McpServer,
@@ -16,9 +12,5 @@ export function registerConfluenceTools(
   registerConfluenceGetPage(server, client);
   registerConfluenceSearch(server, client);
   registerConfluenceGetPageChildren(server, client);
-  registerConfluenceCreatePage(server, client);
-  registerConfluenceUpdatePage(server, client);
-  registerConfluenceUpdatePageDiff(server, client);
   registerConfluenceGetPageComments(server, client);
-  registerConfluenceAddPageComment(server, client);
 }
